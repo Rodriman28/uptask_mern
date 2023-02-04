a4d6a6b28db54977b6091efe092b7bd8256dec68
Rodriman28 <35462876+Rodriman28@users.noreply.github.com>
2023-02-04 00:13:32 -0300

    ObtenerProyecto - EditarProyecto

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -21,10 +21,48 @@ const nuevoProyecto = async (req, res) => {
 const obtenerProyecto = async (req, res) => {
   const { id } = req.params;
 
-  console.log(id);
+  const proyecto = await Proyecto.findById(id);
+
+  if (!proyecto) {
+    const error = new Error("No encontrado");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Acción no válida");
+    return res.status(401).json({ msg: error.message });
+  }
+
+  res.json(proyecto);
 };
 
-const editarProyecto = async (req, res) => {};
+const editarProyecto = async (req, res) => {
+  const { id } = req.params;
+
+  const proyecto = await Proyecto.findById(id);
+
+  if (!proyecto) {
+    const error = new Error("No encontrado");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Acción no válida");
+    return res.status(401).json({ msg: error.message });
+  }
+
+  proyecto.nombre = req.body.nombre || proyecto.nombre;
+  proyecto.descripcion = req.body.descripcion || proyecto.descripcion;
+  proyecto.fechaEntrega = req.body.fechaEntrega || proyecto.fechaEntrega;
+  proyecto.cliente = req.body.cliente || proyecto.cliente;
+
+  try {
+    const proyectoAlmacenado = await proyecto.save();
+    res.json(proyectoAlmacenado);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const eliminarProyecto = async (req, res) => {};
 
