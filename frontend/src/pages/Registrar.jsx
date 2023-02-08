e6dda8e09c3aca5f2f62ff0d30a48c153a7ce9fc
Rodriman28 <35462876+Rodriman28@users.noreply.github.com>
2023-02-08 00:05:16 -0300

    States de registrar

--- frontend/src/pages/Registrar.jsx
+++ frontend/src/pages/Registrar.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Registrar = () => {
+  const [nombre, setNombre] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repetirPassword, setRepetirPassword] = useState("");
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
