ecc72665926e145dc4152b37af2390d5e68ed2a3
Rodriman28 <35462876+Rodriman28@users.noreply.github.com>
2023-02-09 23:34:56 -0300

    Url confirmar cuenta

--- frontend/src/pages/ConfirmarCuenta.jsx
+++ frontend/src/pages/ConfirmarCuenta.jsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+import Alerta from "../components/Alerta";
+
 const ConfirmarCuenta = () => {
+  const params = useParams();
+  const { id } = params;
+
+  useEffect(() => {
+    const confirmarCuenta = async () => {
+      try {
+        const url = `http://localhost:4000/api/usuarios/confirmar/${id}`;
+        const { data } = await axios(url);
+
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    confirmarCuenta();
+  }, []);
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
