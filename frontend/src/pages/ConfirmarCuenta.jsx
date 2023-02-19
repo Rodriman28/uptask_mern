a69e1abb9e1afed8e7f71d6abb3ecb3d787bcda1
Rodriman28 <35462876+Rodriman28@users.noreply.github.com>
2023-02-19 11:29:10 -0300

    Confirmar cuenta y olvide password

--- frontend/src/pages/ConfirmarCuenta.jsx
+++ frontend/src/pages/ConfirmarCuenta.jsx
@@ -39,14 +39,14 @@ const ConfirmarCuenta = () => {
         Confirma tu cuenta y crea tus{" "}
         <span className="text-slate-700">proyectos</span>
       </h1>
-      <div>
+      <div className="mt-20 md:mt-10 shadow-lg px-5 py-10 rounded-xl bg-white">
         {msg && <Alerta alerta={alerta} />}
         {cuentaConfirmada && (
           <Link
             className="block text-center my-5 text-slate-500 uppercase text-sm"
             to="/"
           >
-            ¿Ya tienes una cuenta? Inicia sesión
+            Inicia sesión
           </Link>
         )}
       </div>
--- frontend/src/pages/OlvidePassword.jsx
+++ frontend/src/pages/OlvidePassword.jsx
@@ -1,13 +1,48 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Alerta from "../components/Alerta";
+import axios from "axios";
 
 const OlvidePassword = () => {
+  const [email, setEmail] = useState("");
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (email === "" || email.length < 6) {
+      setAlerta({
+        msg: "El email es obligatorio",
+        error: true,
+      });
+      return;
+    }
+
+    try {
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/api/usuarios/olvide-password`,
+        { email }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const { msg } = alerta;
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
         Recupera tu acceso y no pierdas tus{" "}
         <span className="text-slate-700">proyectos</span>
       </h1>
-      <form className="my-10 bg-white shadow rounded-lg p-10">
+
+      {msg && <Alerta alerta={alerta} />}
+
+      <form
+        className="my-10 bg-white shadow rounded-lg p-10"
+        onSubmit={handleSubmit}
+      >
         <div className="my-5">
           <label
             htmlFor="email"
@@ -20,6 +55,8 @@ const OlvidePassword = () => {
             type="email"
             placeholder="Email de registro"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
